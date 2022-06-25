6f3086bed39852c22b059df002f5f5fb2e373a07
ll782 <laurence.lord@gmail.com>
2022-06-25 17:18:07 +0100

    Fix previous by importing @testing-library

--- discoverRepos/integration.test.tsx
+++ discoverRepos/integration.test.tsx
@@ -1,8 +1,10 @@
-// Hit issue with TS Config for Jest
-//
-// cannot be compiled under '--isolatedModules' because it is considered a global script file. Add an import, export, or an empty 'export {}' statement to make it a module.ts(1208)
+import { render } from '@testing-library/react'
+import Home from '../pages'
 
 describe('Discover Repos', () => {
+  beforeEach(() => {
+    render(<Home />)
+  })
   it('Calls GitHub for repositories created in the last seven days with the highest number of stars ', () => {
     expect(true).toBe(false)
   })
