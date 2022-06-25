f6163ba8da350fc92e7cc3aec9a6c04e63885fe1
ll782 <laurence.lord@gmail.com>
2022-06-25 23:23:00 +0100

    Call GitHub for the most starred recent repos

--- discoverRepos/integration.test.tsx
+++ discoverRepos/integration.test.tsx
@@ -1,11 +1,26 @@
-import { render } from '@testing-library/react'
-import Home from '../pages'
+import { getServerSideProps } from '../pages/index'
+
+const TODAY = '2020-01-30'
+const SEVEN_DAYS_AGO = '2020-01-23'
+jest.useFakeTimers().setSystemTime(new Date(TODAY))
+
+const unmockedFetch = global.fetch
 
 describe('Discover Repos', () => {
-  beforeEach(() => {
-    render(<Home />)
-  })
-  it('Calls GitHub for repositories created in the last seven days with the highest number of stars ', () => {
-    expect(true).toBe(false)
+  describe('When getting server side props', () => {
+    beforeAll(async () => {
+      global.fetch = jest.fn().mockResolvedValue({ json: () => Promise })
+      await getServerSideProps()
+    })
+    afterAll(() => {
+      global.fetch = unmockedFetch
+    })
+    it('Calls GitHub for the most starred, recently created repos ', () => {
+      const searchApi = 'https://api.github.com/search/'
+      const createdQuery = `q=created:%3E${SEVEN_DAYS_AGO}`
+      const highestStarsFirst = 'stars&order=desc'
+      const expectedUrl = `${searchApi}repositories?${createdQuery}&sort=${highestStarsFirst}`
+      expect(fetch).toHaveBeenCalledWith(expectedUrl)
+    })
   })
 })
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
+import dayjs from 'dayjs'
 import Head from 'next/head'
 
 import styles from '@/pages/index.module.css'
+import { SearchReposRes } from 'discoverRepos/model/SearchReposRes'
 
 export default function Home() {
   return (
@@ -15,4 +17,13 @@ export default function Home() {
       </main>
     </div>
   )
+}
+export async function getServerSideProps() {
+  const SEVEN_DAYS_AGO = dayjs().subtract(7, 'day').format('YYYY-MM-DD')
+
+  const res = await fetch(
+    `https://api.github.com/search/repositories?q=created:%3E${SEVEN_DAYS_AGO}&sort=stars&order=desc`
+  )
+  const repos: SearchReposRes = await res.json()
+  return { props: { repos } }
 }
