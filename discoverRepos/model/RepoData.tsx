dfc21dfd44ae34292e8ebff470c6a8a61bc19484
ll782 <laurence.lord@gmail.com>
2022-06-26 13:26:00 +0100

    Note that RepoData.description can be empty

--- discoverRepos/model/RepoData.tsx
+++ discoverRepos/model/RepoData.tsx
@@ -1,7 +1,7 @@
 export type RepoData = {
   full_name: string
   html_url: string
-  description: string
+  description: string | ''
   created_at: string
   stargazers_count: number
   language: string
