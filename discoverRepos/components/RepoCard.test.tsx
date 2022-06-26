9295ea8ca5ac87930272eb55f7cbe10c9ad718ff
ll782 <laurence.lord@gmail.com>
2022-06-26 14:03:30 +0100

    Improve coverage around "favourite" toggle

--- discoverRepos/components/RepoCard.test.tsx
+++ discoverRepos/components/RepoCard.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
 
 import {
   description,
@@ -16,12 +16,16 @@ const data = fakeRepoData
 const fakeProps: Props = { addToFavs, data, isFav, subFromFavs }
 
 const { findByRole, findByText } = screen
-const favToggle = async () => findByRole('checkbox', { name: 'Favourite' })
+const favToggle = async () =>
+  findByRole<HTMLInputElement>('checkbox', { name: 'Favourite' })
 
 describe('Repo card', () => {
   describe('Given complete data', () => {
     beforeEach(() => {
       render(<RepoCard {...fakeProps} />)
+    })
+    afterEach(() => {
+      jest.resetAllMocks()
     })
     it('displays the full name of the repo as a link to the repo', async () => {
       const link: HTMLAnchorElement = await findByRole('link', { name })
@@ -36,6 +40,38 @@ describe('Repo card', () => {
     })
     it('displays a "Favourite" toggle', async () => {
       expect(await favToggle()).toBeInTheDocument()
+    })
+    it('defaults the "Favourite" toggle to unchecked', async () => {
+      expect((await favToggle()).checked).toBe(false)
+    })
+    it('adds the repo to "favourites" if the toggle is clicked', async () => {
+      const toggle = await favToggle()
+      fireEvent.click(toggle)
+      expect(addToFavs).toHaveBeenCalledWith()
+      expect(subFromFavs).not.toHaveBeenCalled()
+    })
+    it('allows toggling repeatedly', async () => {
+      const toggle = await favToggle()
+      fireEvent.click(toggle)
+      fireEvent.click(toggle)
+      fireEvent.click(toggle)
+      expect(addToFavs).toHaveBeenCalledTimes(2)
+      expect(subFromFavs).toHaveBeenCalledTimes(1)
+    })
+  })
+  describe('Given a repo that is in the favourites', () => {
+    beforeEach(() => {
+      isFav.mockReturnValueOnce(true)
+      render(<RepoCard {...fakeProps} />)
+    })
+    it('defaults the "Favourite" toggle to checked', async () => {
+      expect((await favToggle()).checked).toBe(true)
+    })
+    it('subs the repo from "favourites" if the toggle is clicked', async () => {
+      const toggle = await favToggle()
+      fireEvent.click(toggle)
+      expect(subFromFavs).toHaveBeenCalledWith()
+      expect(addToFavs).not.toHaveBeenCalled()
     })
   })
 })
