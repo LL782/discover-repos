9af2a8ca456a14c4a78ac07f6b13f8c6e8fc0438
ll782 <laurence.lord@gmail.com>
2022-06-26 20:23:28 +0100

    Rename "Trending" view (previously "All")

--- discoverRepos/components/ShowResults.tsx
+++ discoverRepos/components/ShowResults.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 
 import { RepoData } from '../model/RepoData'
 import { RepoCard } from './RepoCard'
@@ -7,16 +7,16 @@ interface Props {
   repos: RepoData[]
 }
 
-type View = 'All' | 'Favourites'
+export type View = 'Trending' | 'Favourites'
 
 export const ShowResults = ({ repos }: Props) => {
-  const [view, setView] = useState<View>('All')
+  const [view, setView] = useState<View>('Trending')
   const [favourites, setFavourites] = useState(new Map<string, boolean>())
   const addToFavs = (n: string) => setFavourites(favourites.set(n, true))
   const subFromFavs = (n: string) => setFavourites(favourites.set(n, false))
 
   const reposToShow =
-    view === 'All'
+    view === 'Trending'
       ? repos
       : repos.filter(({ full_name }) => favourites.get(full_name))
 
