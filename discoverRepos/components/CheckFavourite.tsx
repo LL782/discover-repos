4d5c1c534fe3d94a37681dfde7392113cacc2b72
ll782 <laurence.lord@gmail.com>
2022-06-28 20:57:41 +0100

    Style the checkbox label a bit

--- discoverRepos/components/CheckFavourite.tsx
+++ discoverRepos/components/CheckFavourite.tsx
@@ -1,20 +1,29 @@
 import { ChangeEventHandler } from 'react'
 
+import styles from './CheckFavourite.module.css'
+
 interface Props {
   checked: boolean
   id: string
   onChange: ChangeEventHandler<HTMLInputElement>
 }
 
-export const CheckFavourite = ({ checked, id, onChange }: Props) => (
-  <div>
-    <label htmlFor={id}>Favourite</label>
-    <input
-      checked={checked}
-      type="checkbox"
-      onChange={onChange}
-      id={id}
-      name={id}
-    />
-  </div>
-)
+export const CheckFavourite = ({ checked, id, onChange }: Props) => {
+  let className = `${styles.container}`
+  if (checked) className += ` ${styles.checked}`
+  return (
+    <div className={className}>
+      <label className={styles.label} htmlFor={id}>
+        Favourite
+      </label>
+      <input
+        checked={checked}
+        className={styles.input}
+        type="checkbox"
+        onChange={onChange}
+        id={id}
+        name={id}
+      />
+    </div>
+  )
+}
